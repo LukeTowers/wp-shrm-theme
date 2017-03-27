cff781637be37ba28a86e4b599b79b987f925d5b
Luke Towers <luke.towers@lookagency.com>
2017-03-27 15:29:04 -0600

    Changed newsletter signup form to use GravityForms instead of CampaignMonitor

--- includes/modules/template-parts/newsletter-signup.php
+++ includes/modules/template-parts/newsletter-signup.php
@@ -46,67 +46,55 @@ function add_newsletter_subscriber($email) {
 		'error_general'			=>	'There was an error. Contact <a href="mailto:office@shrmsk.com">office@shrmsk.com</a> to sign up for the newsletter.',
 		'error_long_email'		=>	'Your email address is too long.',
 		'error_invalid_email'	=>	'Your email address is invalid.',
+		'error_feed_broken'     =>  'There was an error. Contact <a href="mailto:office@shrmsk.com">office@shrmsk.com</a> to sign up for the newsletter. ERROR: Feed is invalid', 
 		'success'				=>	'Thank you for signing up for our newsletter!',
 	);
-	
-
 	
 	// Validate the email address input
 	if (empty($email)) { return; }
 	if (strlen($email) > 254) { return $result_message['error_long_email']; }
 	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { return $result_message['error_invalid_email']; }
 	
-	
-	
 	// Setup values for accessing backend components
 	$form_id = 1;
-	$feed_id = 9;
 	$email_field_id = 2;
-	$cm_slug = 'gravityformscampaignmonitor';
-	$cm_path = WP_PLUGIN_DIR . '/' . $cm_slug . '/class-gf-campaignmonitor.php';
+	$addon_slug = 'gravityformsmailchimp';
+	$addon_path = WP_PLUGIN_DIR . '/' . $addon_slug . '/class-gf-mailchimp.php';
+	$addon_class = 'GFMailChimp';
 	
-	// Check to make sure that campaign monitor can be loaded
-	if (!file_exists($cm_path)) { return $result_message['error_general']; }
+	// Check to make sure that the addon can be loaded
+	if (!file_exists($addon_path)) { return $result_message['error_general']; }
+	
+	// Get the feed to trigger
+	$feeds = GFAPI::get_feeds(null, $form_id, $addon_slug, true);
+	$target_feed = @$feeds[0];
+	if (!is_array($target_feed) || empty($target_feed)) {
+		return $result_message['error_feed_broken'];
+	}
 	
 	
-	
+		
 	// Build the GF Entry Object we'll be creating
 	$new_entry = array(
 		'form_id'		=>	$form_id,
 		$email_field_id	=>	$email,
 	);
 	
-	
 	// Add the email as a form entry to the database
 	$new_entry_id = GFAPI::add_entry($new_entry);
 	
-	
 	// Build / Get the objects required to send the signup to Campaign Monitor
-	$signup_entry 		=	GFAPI::get_entry($new_entry_id);
-	$newsletter_form	=	GFAPI::get_form($form_id);
+	$entry = GFAPI::get_entry($new_entry_id);
+	$form  = GFAPI::get_form($form_id);
 	
-	// Get the feed from the database and build a feed object with it
-	global $wpdb;
-	$feed_results = $wpdb->get_row('SELECT * FROM ' . $wpdb->prefix . 'gf_addon_feed' . ' WHERE id = ' . $feed_id);
-	
-	$cm_feed = array(
-		'id'		=>	$feed_id,
-		'form_id'	=>	$form_id,
-		'is_active'	=>	true,
-		'meta'		=>	(array) json_decode($feed_results->meta),
-		'addon_slug'=>	$cm_slug,
-	);
-	
-	
-	// Attempt to process the newsletter signup
-	require_once($cm_path);
-	if (class_exists('GFCampaignMonitor')) {
-		$campaign_monitor = new GFCampaignMonitor();
-		$campaign_monitor->export_feed($signup_entry, $newsletter_form, $cm_feed);
+	// Attempt to process the newsletter signup - note, will succeed on API failures
+	require_once($addon_path);
+	if (class_exists($addon_class)) {
+		$addon = $addon_class::get_instance();
+		$addon->process_feed($target_feed, $entry, $form);
 	} else {
 		return $result_message['error_general'];
 	}
-	
 	
 	// If everything has worked properly
 	return $result_message['success'];
